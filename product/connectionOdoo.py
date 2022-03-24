f979bf1ba2afd6a084af19b6c4b05718d9fcc0ff
cristellnaranjo <cristellnaranjoesponda31@gmail.com>
2022-03-24 17:45:59 -0600

    Add api Telcel

---
+++ product/connectionOdoo.py
@@ -0,0 +1,43 @@
+from contextlib import nullcontext
+from itertools import product
+from django.shortcuts import render
+from django.http import HttpResponse, response
+# import requests
+import xmlrpc.client
+import json
+import base64
+import sys
+import json 
+import os
+import socket
+
+from dotenv import load_dotenv
+load_dotenv()
+class connectionOdoo():
+    models = None
+    common = None
+    output = None
+    uid = None
+    url =  ""
+    db = ""
+    password = ""
+    user = ""
+    def __init__(self, *args):
+
+
+        self.url = os.getenv("URL_ODOO")
+        self.db =  os.getenv("DB_ODOO")
+        self.password =  os.getenv("PASSWORD_ODOO")
+        self.user =  os.getenv("USERNAME_ODOO")
+
+
+        self.models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(self.url))
+
+        self.common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(self.url))
+
+        self.output = self.common.version()
+        
+        self.uid = self.common.authenticate(self.db,self.user , self.password, self.output)
+
+        
+        
---
+++ product/views.py
@@ -0,0 +1,114 @@
+from itertools import product
+from django.shortcuts import render
+from django.http import HttpResponse, response
+# import requests
+import xmlrpc.client
+import json
+import base64
+import sys
+import json 
+import os
+import pandas as pd
+import csv
+
+
+ 
+from .connectionOdoo import * 
+ 
+from dotenv import load_dotenv
+load_dotenv()
+# from dotenv import load_dotenv
+ 
+# Create your views here.
+def viewproducts(request):
+    #return HttpResponse(socket.gethostbyname(socket.gethostname()))
+    
+    odoo = connectionOdoo()
+    products =  odoo.models.execute_kw(
+    odoo.db
+    ,odoo.uid,
+    odoo.password,
+    'product.template','search_read',
+    [
+        [
+            [ "company_id" ,"=", 4]
+            # [ "id" ,"=", 2]
+        ]
+    ],
+    {'fields': []}
+)
+ 
+    data = {
+        "products":products
+    }
+    
+        
+    # return HttpResponse(xx)
+    return render(request ,"products/list.html",data)
+ 
+def createProduct(request):
+    url = os.getenv("URL_ODOO")
+    db =  os.getenv("DB_ODOO")
+    password =  os.getenv("PASSWORD_ODOO")
+ 
+    models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    output = common.version()
+    uid = common.authenticate(db, os.getenv("USERNAME_ODOO"), password, output)
+ 
+    id = models.execute_kw(db, uid, password, 'product.template', 'create', [{
+    'name': "Producto DJANGO companyID",
+    'default_code':"test_default_code_DJANGO",
+    'list_price':"100",
+    "company_id":1
+    }])
+    return HttpResponse(id)
+ 
+def deleteProduct(request, id):
+    url = os.getenv("URL_ODOO")
+    db =  os.getenv("DB_ODOO")
+    password =  os.getenv("PASSWORD_ODOO")
+    models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    output =  common.version()
+    uid = common.authenticate(db, os.getenv("USERNAME_ODOO"), password, output)
+    models.execute_kw(db, uid, password, 'product.template', 'unlink', [[id]])
+    return HttpResponse("deleted")
+ 
+def updateProduct(request):
+    odoo = connectionOdoo()
+    odoo.models.execute_kw(odoo.db, odoo.uid, odoo.password, 'product.template', 'write', [[43], {
+    'name': "Newer partner",
+    "price":"4567"
+        }])
+    return HttpResponse("updated")
+    
+def createCsvProducts(name, defaultCode, listPrice, companyId):
+    self = connectionOdoo()
+    id = self.models.execute_kw(self.db, self.uid, self.password, 'product.template', 'create',
+                                [
+                                    {
+                                        'name': name,
+                                        'default_code': defaultCode,
+                                        'list_price': listPrice,
+                                        'company_id': companyId
+                                    }
+                                ])
+    
+
+def AddProductsCSV(request):
+    csv_file = os.path.join(os.path.dirname(__file__), 'productos.csv')
+    df = pd.read_csv(csv_file)
+
+    print(df.to_numpy()[0])
+    for i in range(len(df.to_numpy())):
+        createCsvProducts(df.to_numpy()[i][0], df.to_numpy()[i][1], df.to_numpy()[i][2], df.to_numpy()[i][3])
+    return HttpResponse("CVS agregado")
+
+    return HttpResponse("No hay archivo")
+
+
+# check if the deleted record is still in the database
+    # models.execute_kw(db, uid, password,
+    #     'res.partner', 'search', [[['id', '=', id]]])
+   
